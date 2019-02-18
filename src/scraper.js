a4c985834fed5497d8fbe2d59e337a57f6735cde
Estevan Maito <ejmaito@gmail.com>
2019-02-18 19:09:39 -0300

    initial commit

---
+++ src/scraper.js
@@ -0,0 +1,27 @@
+const rp = require('request-promise')
+const $ = require('cheerio')
+
+const URL = 'https://gizmodo.uol.com.br'
+
+async function getNews() {
+  return await rp(URL)
+    .then(function(body) {
+      let result = Array.from($('.list-item', body))
+
+      let news = result.map(i => ({
+        title: $(i).find('.postTitle > a').text(),
+        description: $(i).find('.postSummary > p').text(),
+        author: $(i).find('.postMeta--author-author > a').text(),
+        date: $(i).find('.published').attr('title'),
+        image: $(i).find('.wp-post-image').attr('src'),
+        href: $(i).find('.o-overlayLink').attr('href'),
+      }))
+
+      return news
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+}
+
+module.exports = getNews
