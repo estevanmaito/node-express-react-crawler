7a8d17938025006f9d2b6ca9bcc3dee0503d9c30
Estevan Maito <ejmaito@gmail.com>
2019-02-19 16:23:23 -0300

    add styles

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import Head from 'next/head'
+import '../static/css/card.css'
+
 const Index = ({news}) => (
-  <div>
+  <div className="container">
+    <Head>
+      <meta name="viewport" content="initial-scale=1 width=device-width" />
+    </Head>
     {
       news.map(n => (
-        <h1 key={n.title}>{n.title}</h1>
+        <a href={n.href} target="_blank" rel="noopener noreferrer" className="card" key={n.title} style={{ backgroundImage: `url(${n.image})` }}>
+          <h3 className="card__title">{n.title}</h3>
+          <small className="card__date">{n.date}</small>
+          <p className="card__desc">{n.description}</p>
+          <div className="overlay"></div>
+        </a>
       ))
     }
   </div>
